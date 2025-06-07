555cfbe2551c38402e1e9f4f962b339666a05509
picuu <carloscapoforcada@gmail.com>
2025-06-07 18:33:12 +0200

    feat: upload icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,33 @@
+import BellIcon from '@/assets/icons/bell-icon.svg'
+import CheckIcon from '@/assets/icons/check-icon.svg'
+import ChevronDownIcon from '@/assets/icons/chevron-down-icon.svg'
+import FacebookIcon from '@/assets/icons/facebook-icon.svg'
+import InfoIcon from '@/assets/icons/info-icon.svg'
+import InstagramIcon from '@/assets/icons/instagram-icon.svg'
+import MaturingRating16Icon from '@/assets/icons/maturing-rating-16-icon.svg'
+import PlayIcon from '@/assets/icons/play-icon.svg'
+import PlusIcon from '@/assets/icons/plus-icon.svg'
+import SearchIcon from '@/assets/icons/search-icon.svg'
+import ThumbsDownIcon from '@/assets/icons/thumbs-down-icon.svg'
+import ThumbsUpIcon from '@/assets/icons/thumbs-up-icon.svg'
+import ThumbsUpTwoIcon from '@/assets/icons/thumbs-up-two-icon.svg'
+import TwitterIcon from '@/assets/icons/twitter-icon.svg'
+import YoutubeIcon from '@/assets/icons/youtube-icon.svg'
+
+export {
+  SearchIcon,
+  BellIcon,
+  FacebookIcon,
+  TwitterIcon,
+  InstagramIcon,
+  YoutubeIcon,
+  PlayIcon,
+  InfoIcon,
+  ChevronDownIcon,
+  MaturingRating16Icon,
+  PlusIcon,
+  ThumbsUpTwoIcon,
+  ThumbsUpIcon,
+  ThumbsDownIcon,
+  CheckIcon
+}

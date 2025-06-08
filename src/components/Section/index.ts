5ccd4cdc35732956373d3660486fe7f1f964ed7d
picuu <carloscapoforcada@gmail.com>
2025-06-08 18:44:11 +0200

    feat: add Section, SectionContent, and SectionTitle components

---
+++ src/components/Section/index.ts
@@ -0,0 +1,5 @@
+import Section from './Section.astro'
+import SectionContent from './SectionContent.astro'
+import SectionTitle from './SectionTitle.astro'
+
+export { Section, SectionTitle, SectionContent }
